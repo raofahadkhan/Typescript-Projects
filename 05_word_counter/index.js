981bcf75af0f7cccbb265974b059b95bc0be6e7d
RAO FAHAD <raofahad046@gmail.com>
2023-08-05 09:03:37 +0500

    completed project 5

---
+++ 05_word_counter/index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter Your Sentence to Get the Word Count: ",
+    },
+]);
+const words = answers.sentence
+    .trim()
+    .split(" ")
+    .filter((num) => num != "");
+console.log(`Your sentence word count is ${words.length}`);
---
+++ 05_word_counter/index.ts
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+
+const answers: { sentence: string } = await inquirer.prompt([
+	{
+		name: "sentence",
+		type: "input",
+		message: "Enter Your Sentence to Get the Word Count: ",
+	},
+]);
+
+const words = answers.sentence
+	.trim()
+	.split(" ")
+	.filter((num) => num != "");
+console.log(`Your sentence word count is ${words.length}`);
