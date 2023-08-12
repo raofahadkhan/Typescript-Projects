29c170562a48d00cec040693c1a6092e6a4777f5
RAO FAHAD <raofahad046@gmail.com>
2023-08-12 22:12:03 +0500

    completed project 11 OOP MY BANK

--- 06_student_managment_system/index.ts
+++ 06_student_managment_system/index.ts
@@ -1,3 +1,101 @@
+// class Person {
+// 	name: string;
+// 	age: number;
+
+// 	constructor(name: string, age: number) {
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+
+// 	getName() {
+// 		return this.name;
+// 	}
+// }
+
+// class Student extends Person {
+// 	rollNumber: string;
+// 	courses: Course[] = [];
+
+// 	constructor(name: string, age: number, rollNumber: string) {
+// 		super(name, age);
+// 		this.rollNumber = rollNumber;
+// 	}
+
+// 	registerForCourses(course: Course) {
+// 		this.courses.push(course);
+// 	}
+// }
+
+// class Instructor extends Person {
+// 	private salary: number;
+// 	courses: Course[] = [];
+
+// 	constructor(name: string, age: number, salary: number) {
+// 		super(name, age);
+// 		this.salary = salary;
+// 	}
+
+// 	assignCourse(course: Course) {
+// 		this.courses.push(course);
+// 	}
+// }
+
+// class Course {
+// 	id: string;
+// 	name: string;
+// 	students: Student[] = [];
+// 	instructor!: Instructor;
+
+// 	constructor(id: string, name: string) {
+// 		this.id = id;
+// 		this.name = name;
+// 	}
+
+// 	addStudent(student: Student) {
+// 		this.students.push(student);
+// 		student.registerForCourses(this);
+// 	}
+
+// 	setInstructor(instructor: Instructor) {
+// 		this.instructor = instructor;
+// 		instructor.assignCourse(this);
+// 	}
+// }
+
+// class Department {
+// 	name: string;
+// 	courses: Course[] = [];
+
+// 	constructor(name: string) {
+// 		this.name = name;
+// 	}
+
+// 	addCourse(course: Course) {
+// 		this.courses.push(course);
+// 	}
+// }
+
+// const course1 = new Course("course1", "Metaverse");
+// const course2 = new Course("course2", "Blockchain");
+
+// const department1 = new Department("Computer Science")
+// department1.addCourse(course1)
+// department1.addCourse(course2)
+
+// const student1 = new Student("Fahad", 23, "025793");
+// const student2 = new Student("Asim", 20, "12345");
+
+// const instructor1 = new Instructor("Nasir Hussain", 35, 100000);
+// const instructor2 = new Instructor("Qasim Ferozpurwala", 45, 100000);
+
+// course1.addStudent(student1);
+// course1.addStudent(student2);
+// course1.setInstructor(instructor1);
+// course1.setInstructor(instructor2);
+
+// console.log(student1);
+// console.log(student1.courses);
+
 class Person {
 	name: string;
 	age: number;
@@ -21,14 +119,14 @@ class Student extends Person {
 		this.rollNumber = rollNumber;
 	}
 
-	registerForCourses(course: Course) {
+	registerForCourse(course: Course) {
 		this.courses.push(course);
 	}
 }
 
 class Instructor extends Person {
 	private salary: number;
-	courses: Course[] = []; 
+	courses: Course[] = [];
 
 	constructor(name: string, age: number, salary: number) {
 		super(name, age);
@@ -44,7 +142,7 @@ class Course {
 	id: string;
 	name: string;
 	students: Student[] = [];
-	instructor!: Instructor;
+	instructors!: Instructor[];
 
 	constructor(id: string, name: string) {
 		this.id = id;
@@ -53,11 +151,11 @@ class Course {
 
 	addStudent(student: Student) {
 		this.students.push(student);
-		student.registerForCourses(this);
+		student.registerForCourse(this);
 	}
 
 	setInstructor(instructor: Instructor) {
-		this.instructor = instructor;
+		this.instructors.push(instructor);
 		instructor.assignCourse(this);
 	}
 }
@@ -74,24 +172,3 @@ class Department {
 		this.courses.push(course);
 	}
 }
-
-const course1 = new Course("course1", "Metaverse");
-const course2 = new Course("course2", "Blockchain");
-
-const department1 = new Department("Computer Science")
-department1.addCourse(course1)
-department1.addCourse(course2)
-
-const student1 = new Student("Fahad", 23, "025793");
-const student2 = new Student("Asim", 20, "12345");
-
-const instructor1 = new Instructor("Nasir Hussain", 35, 100000);
-const instructor2 = new Instructor("Qasim Ferozpurwala", 45, 100000);
-
-course1.addStudent(student1);
-course1.addStudent(student2);
-course1.setInstructor(instructor1);
-course1.setInstructor(instructor2);
-
-console.log(student1);
-console.log(student1.courses);
